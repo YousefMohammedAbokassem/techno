570bfa78aa27894f8bceb2e06387a2f58c002214
yousef <youseff2000q@gmail.com>
2024-03-20 15:04:00 +0300

    upload

--- src/app/[lng]/components/Mobile/Mobile.jsx
+++ src/app/[lng]/components/Mobile/Mobile.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import HeadSec from "../HeadSec/HeadSec";
-import { useTranslation } from "@/app/i18n/client";
 // Import Swiper React components
 import SwiperComp from "../../components/Swiper/SwiperSlider";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -19,6 +18,7 @@ import "swiper/css/effect-cards";
 // import required modules
 import { Pagination, Navigation, EffectCards } from "swiper/modules";
 import LottieSwip from "./LottieSwip";
+import { useTranslation } from "@/app/i18n/client";
 
 export default function Mobile({ lng }) {
   const [activeSlideIndex, setActiveSlideIndex] = useState(null);
@@ -27,13 +27,17 @@ export default function Mobile({ lng }) {
   const [images, setImages] = useState([]);
   const [popup, setPopup] = useState(false);
   const fetchData = async () => {
-    const res = await axios.get(`back.portfolio.technoplus.dev/api/user/apps`);
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user/apps`);
     setData(res.data.apps);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
+  const navigationOptions = {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  };
   // console.log(data?.map((item) => console.log(item.id)));
   return (
     <div
@@ -43,39 +47,30 @@ export default function Mobile({ lng }) {
       id="ui"
     >
       <HeadSec info={"uiUx"} />
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="fullSlider"
-      >
-        {data?.map((item) => {
-          return (
-            <SwiperSlide className="w-full" key={item.id}>
-              <div className="info flex lg:flex-row flex-col items-center justify-around gap-6 lg:p-0 p-5">
-                <div className="image h-[400px] lg:h-[450px]">
-                  {item.main_image === undefined ? (
-                    ""
-                  ) : (
-                    //#########################################
-                    // <img
-                    //   // src={`back.portfolio.technoplus.dev${item.main_image}`}
-                    //   src={`${item.main_image}`}
-                    //   className="w-full h-full"
-                    //   width={450}
-                    //   height={450}
-                    //   alt="no image"
-                    // />
+      <div className="m-[25px]">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          // navigation={true}
+          navigation={navigationOptions}
+          modules={[Pagination, Navigation]}
+          className="fullSlider"
+        >
+          {data?.map((item) => {
+            return (
+              <SwiperSlide className="w-full" key={item.id}>
+                <div className="info flex lg:flex-row flex-col items-center justify-around gap-6 lg:p-0 p-5">
+                  <div className="image h-[400px] lg:h-[450px]">
                     <>
                       <Swiper
                         effect={"cards"}
                         grabCursor={true}
                         modules={[EffectCards]}
+                        nested={true}
                         className="mySwiperCards relative"
                         cardsEffect={{
                           slideShadows: false,
@@ -88,29 +83,32 @@ export default function Mobile({ lng }) {
                         {item?.images?.map((image) => {
                           return (
                             <SwiperSlide key={image.id}>
-                              <Image
-                                width={450}
-                                height={450}
-                                src={`back.portfolio.technoplus.dev${image.image_url}`}
-                                alt=""
-                              />
+                              {image.image_url === undefined ? (
+                                ""
+                              ) : (
+                                <Image
+                                  width={450}
+                                  height={450}
+                                  src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${image.image_url}`}
+                                  alt=""
+                                />
+                              )}
                             </SwiperSlide>
                           );
                         })}
                       </Swiper>
                     </>
-                  )}
-                </div>
-                <div className="flex flex-col items-center justify-between text-center gap-5 lg:gap-9 lg:w-1/2 w-full md:p-5 lg:p-0 p-3 mt-[80px] lg:mt-0">
-                  <h2 className="text-3xl md:text-2xl font-semibold">
-                    {item.name}
-                  </h2>
+                  </div>
+                  <div className="flex flex-col items-center justify-between text-center gap-5 lg:gap-9 lg:w-1/2 w-full md:p-5 lg:p-0 p-3 mt-[80px] lg:mt-0">
+                    <h2 className="text-3xl md:text-2xl font-semibold">
+                      {item.name}
+                    </h2>
 
-                  <p className="lg:text-2xl text-lg leading-8">
-                    {item.description}
-                  </p>
-                  {/* <Link href="ViewMobile"> */}
-                  {/* <button
+                    <p className="lg:text-2xl text-lg leading-8">
+                      {item.description}
+                    </p>
+                    {/* <Link href="ViewMobile"> */}
+                    {/* <button
                     type="button"
                     className="text-[#0090FF] bg-white px-3 py-1 cursor-pointer rounded-full font-semibold"
                     onClick={() => {
@@ -126,14 +124,32 @@ export default function Mobile({ lng }) {
                   >
                     {t("view")}
                   </button> */}
-                  {/* </Link> */}
+                    {/* </Link> */}
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-      {/* {popup && (
+              </SwiperSlide>
+            );
+          })}
+          <div className="swiper-button-next bg-white rounded-full w-[30px] h-[30px] top-1/2 toRight">
+            <Image
+              src="/Group-20.png"
+              alt=""
+              width={75}
+              height={75}
+              className="w-[95%] h-[95%] translate-x-[-16%]"
+            />
+          </div>
+          <div className="swiper-button-prev bg-white rounded-full w-[30px] h-[30px] top-1/2  toLeft">
+            <Image
+              src="/Group-13.png"
+              alt=""
+              width={75}
+              height={75}
+              className="w-[95%] h-[95%] translate-x-[16%]"
+            />
+          </div>
+        </Swiper>
+        {/* {popup && (
         <div className="z-10 overflow-hidden swiperMobile fixed left-0 top-0 h-screen w-full">
           <div className="relative container mx-auto">
             <SwiperComp images={images} />
@@ -146,6 +162,7 @@ export default function Mobile({ lng }) {
           </div>
         </div>
       )} */}
+      </div>
     </div>
   );
 }
