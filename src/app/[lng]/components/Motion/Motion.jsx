9f6419d8560815002b036023b7efcde32bf25000
yousef <youseff2000q@gmail.com>
2024-03-22 00:51:15 +0300

    upload

--- src/app/[lng]/components/Motion/Motion.jsx
+++ src/app/[lng]/components/Motion/Motion.jsx
@@ -3,14 +3,14 @@ import SwiperCompVideos from "../Swiper/SwiperCompVideos";
 import HeadSec from "../HeadSec/HeadSec";
 import axios from "axios";
 export default async function Motion({ lng }) {
-  const fetchData = await axios.get(
+  const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/user/videos`,
     {
       cache: "no-store",
     }
   );
-  // console.log(fetchData.data.designs);
-  let images = fetchData.data.videos;
+  const fetchData = await response.json();
+  let images = fetchData?.videos;
   return (
     <div
       className="py-12 relative z-10 overflow-hidden container mx-auto"
--- src/app/[lng]/components/Social/Social.jsx
+++ src/app/[lng]/components/Social/Social.jsx
@@ -5,14 +5,22 @@ import axios from "axios";
 
 export default async function Social({ lng }) {
   //  process.env.NEXT_PUBLIC_API_URL;
-  const fetchData = await axios.get(
+  // const fetchData = await axios.get(
+  //   `${process.env.NEXT_PUBLIC_API_URL}/user/socialMedia`,
+  //   {
+  //     cache: "no-store",
+  //   }
+  // );
+  // // console.log(fetchData.data);
+  // let images = fetchData.data.designs;
+  const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/user/socialMedia`,
     {
       cache: "no-store",
     }
   );
-  // console.log(fetchData.data);
-  let images = fetchData.data.designs;
+  const fetchData = await response.json();
+  let images = fetchData?.designs;
   return (
     <div
       className="py-12 relative z-10 overflow-hidden container mx-auto"
